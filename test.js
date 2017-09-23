faccdffafec4eeb06c6b0d514694bc109f3cd833
AnthonyA88 <Anthonydarata@gmail.com>
2017-09-22 20:50:48 -0700

    test project

---
+++ test.js
@@ -0,0 +1,14 @@
+//the first alert
+alert("Hello World");
+
+//second alert
+alert("Hello again");
+
+var youLikeFootball = false;
+
+if (youLikeFootball) {
+
+	document.write("Here are the games for sunday");
+}
+
+var firstName = prompt("What is your first name?"); var lastName = prompt("What is your last name?"); alert(firstName + ' ' + lastName);
